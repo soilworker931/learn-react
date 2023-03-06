3eaaa7e3b28756dae4e1d377955bd5e15bb773a6
German Gurinovich <hermanhurynovich@gmail.com>
2023-03-06 19:53:22 +0100

    make drag feature

--- src/events/EventComponent.tsx
+++ src/events/EventComponent.tsx
@@ -5,9 +5,16 @@ const EventComponent: React.FC = () => {
     console.log(event);
   };
 
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>): void => {
+    console.log(event);
+  };
+
   return (
     <div>
       <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}>
+        Drag Me!
+      </div>
     </div>
   );
 };
